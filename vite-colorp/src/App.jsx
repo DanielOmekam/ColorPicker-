e783119d04d77c0931f8c45810e0ef67e5980912
Daniel Omekam <omekam.daniel@gmail.com>
2024-10-01 15:11:23 -0400

    colorp

--- vite-colorp/src/App.jsx
+++ vite-colorp/src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
+
+const [selectedColor, setSelectedColor] = useState("")
+
 // Write your Color component here
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
 
 const App = () => {
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color = "yellow" setSelectedColor={setSelectedColor} />
+        <Color color = "green" setSelectedColor={setSelectedColor} />
+        <Color color = "black" setSelectedColor={setSelectedColor} />
+      </div>
+
+      
     </div>
   );
 };
