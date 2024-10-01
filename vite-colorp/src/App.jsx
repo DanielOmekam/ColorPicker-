f7b2813325908e50b5d16d7c7521213c3649e346
Daniel Omekam <omekam.daniel@gmail.com>
2024-10-01 19:14:16 -0400

    colorp

--- vite-colorp/src/App.jsx
+++ vite-colorp/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
-const [selectedColor, setSelectedColor] = useState("")
+const Color = ({ color, selectedColor, setSelectedColor }) => {
+  // ternary operator to compare the "selectedColor" on state to the "color" prop that each Color receives
+  const isSelected = selectedColor === color ? "selected" : "";
 
-// Write your Color component here
-const Color = ({ color, setSelectedColor }) => {
-  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+  return (
+    <div
+      // isSelected is used as a className and will be either "selected" or empty string
+      className={`${color} ${isSelected}`}
+      onClick={() => setSelectedColor(color)}
+    />
+  );
 };
-
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
@@ -15,12 +23,23 @@ const App = () => {
         <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color = "yellow" setSelectedColor={setSelectedColor} />
-        <Color color = "green" setSelectedColor={setSelectedColor} />
-        <Color color = "black" setSelectedColor={setSelectedColor} />
+        {/* pass the selected color as prop to Color Component */}
+        <Color
+          color="yellow"
+          selectedColor={selectedColor}
+          setSelectedColor={setSelectedColor}
+        />
+        <Color
+          color="green"
+          selectedColor={selectedColor}
+          setSelectedColor={setSelectedColor}
+        />
+        <Color
+          color="black"
+          selectedColor={selectedColor}
+          setSelectedColor={setSelectedColor}
+        />
       </div>
-
-      
     </div>
   );
 };
